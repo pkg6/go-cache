52cdc1020dbc33b2498a460c6be72f9c1e0b626c
chihqiang <zhiqiang2033@gmail.com>
2023-05-27 00:35:08 +0800

    feat:fileCache

---
+++ file_test.go
@@ -0,0 +1,191 @@
+package cache
+
+import (
+	"context"
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"os"
+	"path/filepath"
+	"testing"
+	"time"
+)
+
+func TestFileCacheGet(t *testing.T) {
+	testCases := []struct {
+		name    string
+		key     string
+		value   string
+		cache   Cache
+		wantErr error
+	}{
+		{
+			name:  "get val",
+			key:   "key1",
+			value: "author",
+			cache: func() Cache {
+				bm, err := NewFileCache(
+					FileCacheWithCachePath("cache"),
+					FileCacheWithFileSuffix(".bin"),
+					FileCacheWithEmbedExpiry(0))
+				assert.Nil(t, err)
+				err = bm.Set(context.Background(), "key1", "author", 5*time.Second)
+				assert.Nil(t, err)
+				return bm
+			}(),
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			val, err := tc.cache.Get(context.Background(), tc.key)
+			assert.Nil(t, err)
+			assert.Equal(t, tc.value, val)
+		})
+	}
+	assert.Nil(t, os.RemoveAll("cache"))
+}
+
+func TestFileCacheIsExist(t *testing.T) {
+	cache, err := NewFileCache(
+		FileCacheWithCachePath("cache"),
+		FileCacheWithFileSuffix(".bin"),
+		FileCacheWithEmbedExpiry(0))
+	assert.Nil(t, err)
+	testCases := []struct {
+		name            string
+		key             string
+		value           string
+		timeoutDuration time.Duration
+		isExist         bool
+	}{
+		{
+			name:            "expired",
+			key:             "key0",
+			value:           "value0",
+			timeoutDuration: 1 * time.Second,
+			isExist:         true,
+		},
+		{
+			name:            "exist",
+			key:             "key1",
+			value:           "author",
+			timeoutDuration: 5 * time.Second,
+			isExist:         true,
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			err := cache.Set(context.Background(), tc.key, tc.value, tc.timeoutDuration)
+			assert.Nil(t, err)
+			time.Sleep(2 * time.Second)
+		})
+	}
+	assert.Nil(t, os.RemoveAll("cache"))
+}
+
+func TestFileCacheDelete(t *testing.T) {
+	cache, err := NewFileCache(
+		FileCacheWithCachePath("cache"),
+		FileCacheWithFileSuffix(".bin"),
+		FileCacheWithEmbedExpiry(0))
+	assert.Nil(t, err)
+	testCases := []struct {
+		name            string
+		key             string
+		value           string
+		timeoutDuration time.Duration
+	}{
+		{
+			name:            "delete val",
+			key:             "key1",
+			value:           "author",
+			timeoutDuration: 5 * time.Second,
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			err := cache.Set(context.Background(), tc.key, tc.value, tc.timeoutDuration)
+			assert.Nil(t, err)
+			err = cache.Delete(context.Background(), tc.key)
+			assert.Nil(t, err)
+		})
+	}
+	assert.Nil(t, os.RemoveAll("cache"))
+}
+
+func TestFileCacheGetMulti(t *testing.T) {
+	cache, err := NewFileCache(
+		FileCacheWithCachePath("cache"),
+		FileCacheWithFileSuffix(".bin"),
+		FileCacheWithEmbedExpiry(0))
+	assert.Nil(t, err)
+	testCases := []struct {
+		name            string
+		keys            []string
+		values          []any
+		timeoutDuration time.Duration
+	}{
+		{
+			name:            "key expired",
+			keys:            []string{"key0", "key1"},
+			values:          []any{"value0", "value1"},
+			timeoutDuration: 1 * time.Second,
+		},
+		{
+			name:            "get multi val",
+			keys:            []string{"key2", "key3"},
+			values:          []any{"value2", "value3"},
+			timeoutDuration: 5 * time.Second,
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			for idx, key := range tc.keys {
+				value := tc.values[idx]
+				err := cache.Set(context.Background(), key, value, tc.timeoutDuration)
+				assert.Nil(t, err)
+			}
+			time.Sleep(2 * time.Second)
+			vals, err := cache.GetMulti(context.Background(), tc.keys)
+			if err != nil {
+				assert.ErrorContains(t, err, ErrKeyExpired.Error())
+				return
+			}
+			assert.Equal(t, tc.values, vals)
+		})
+	}
+	assert.Nil(t, os.RemoveAll("cache"))
+}
+
+func TestFileGetContents(t *testing.T) {
+	_, err := os.ReadFile("/bin/aaa")
+	assert.NotNil(t, err)
+	fn := filepath.Join(os.TempDir(), "fileCache.txt")
+	f, err := os.Create(fn)
+	assert.Nil(t, err)
+	_, err = f.WriteString("text")
+	assert.Nil(t, err)
+	data, err := os.ReadFile(fn)
+	assert.Nil(t, err)
+	assert.Equal(t, "text", string(data))
+}
+
+func TestGobEncodeDecode(t *testing.T) {
+	_, err := GobEncode(func() {
+		fmt.Print("test func")
+	})
+	assert.NotNil(t, err)
+	data, err := GobEncode(&CacheItem{
+		Data: "hello",
+	})
+	assert.Nil(t, err)
+	err = GobDecode([]byte("wrong data"), &CacheItem{})
+	assert.NotNil(t, err)
+	dci := &CacheItem{}
+	err = GobDecode(data, dci)
+	assert.Nil(t, err)
+	assert.Equal(t, "hello", dci.Data)
+}
+
+func getTestCacheFilePath() string {
+	return filepath.Join(os.TempDir(), "test", "file.txt")
+}
---
+++ util.go
@@ -0,0 +1,123 @@
+package cache
+
+import (
+	"bytes"
+	"encoding/gob"
+	"errors"
+	"fmt"
+	"math"
+)
+
+var (
+	ErrIncrementOverflow = errors.New("this incr invocation will overflow")
+	ErrDecrementOverflow = errors.New("this decr invocation will overflow")
+	ErrNotIntegerType    = errors.New("item val is not (u)int (u)int32 (u)int64")
+)
+
+// GobEncode Gob encodes a file cache item.
+func GobEncode(data any) ([]byte, error) {
+	buf := bytes.NewBuffer(nil)
+	enc := gob.NewEncoder(buf)
+	err := enc.Encode(data)
+	if err != nil {
+		return nil, errors.New("could not encode this data")
+	}
+	return buf.Bytes(), nil
+}
+
+// GobDecode Gob decodes a file cache item.
+func GobDecode(data []byte, to *CacheItem) error {
+	buf := bytes.NewBuffer(data)
+	dec := gob.NewDecoder(buf)
+	err := dec.Decode(&to)
+	if err != nil {
+		return errors.New("could not decode this data to FileCacheItem. Make sure that the data is encoded by GOB")
+	}
+	return nil
+}
+
+func unwrapF(format string, a ...any) error {
+	return errors.Unwrap(errors.New(fmt.Sprintf(format, a...)))
+}
+func wrapF(format string, a ...any) error {
+	return errors.New(fmt.Sprintf(format, a...))
+}
+
+// Decrement Self decrement
+func Decrement(originVal any, step int) (any, error) {
+	switch val := originVal.(type) {
+	case int:
+		tmp := val - step
+		if val < 0 && tmp > 0 {
+			return nil, ErrDecrementOverflow
+		}
+		return tmp, nil
+	case int32:
+		if val == math.MinInt32 {
+			return nil, ErrDecrementOverflow
+		}
+		return val - int32(step), nil
+	case int64:
+		if val == math.MinInt64 {
+			return nil, ErrDecrementOverflow
+		}
+		return val - int64(step), nil
+	case uint:
+		if val == 0 {
+			return nil, ErrDecrementOverflow
+		}
+		return val - uint(step), nil
+	case uint32:
+		if val == MinUint32 {
+			return nil, ErrDecrementOverflow
+		}
+		return val - uint32(step), nil
+	case uint64:
+		if val == MinUint64 {
+			return nil, ErrDecrementOverflow
+		}
+		return val - uint64(step), nil
+	default:
+		return nil, ErrNotIntegerType
+	}
+}
+
+// Increment Autoincrement
+func Increment(originVal any, step int) (any, error) {
+	switch val := originVal.(type) {
+	case int:
+		tmp := val + step
+		if val > 0 && tmp < 0 {
+			return nil, ErrIncrementOverflow
+		}
+		return tmp, nil
+	case int32:
+		if val == math.MaxInt32 {
+			return nil, ErrIncrementOverflow
+		}
+		return val + int32(step), nil
+	case int64:
+		if val == math.MaxInt64 {
+			return nil, ErrIncrementOverflow
+		}
+		return val + int64(step), nil
+	case uint:
+		tmp := val + 1
+		if tmp < val {
+			return nil, ErrIncrementOverflow
+		}
+		return tmp, nil
+	case uint32:
+		if val == math.MaxUint32 {
+			return nil, ErrIncrementOverflow
+		}
+		return val + uint32(step), nil
+	case uint64:
+		if val == math.MaxUint64 {
+			return nil, ErrIncrementOverflow
+		}
+		return val + uint64(step), nil
+	default:
+		return nil, ErrNotIntegerType
+	}
+}
