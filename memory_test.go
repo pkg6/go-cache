7ae6e1474618d31425564f5a66acd735c417e593
chikkeung <zhiqiangwang@aliyun.com>
2023-05-27 09:16:22 +0800

    feat:MemoryCache

---
+++ memory_test.go
@@ -0,0 +1,81 @@
+package cache
+
+import (
+	"context"
+	"github.com/stretchr/testify/assert"
+	"sync"
+	"testing"
+	"time"
+)
+
+func TestMemoryCacheGet(t *testing.T) {
+	testCases := []struct {
+		name    string
+		key     string
+		value   string
+		cache   Cache
+		wantErr error
+	}{
+		{
+			name:    "key not exist",
+			key:     "key0",
+			wantErr: ErrKeyNotExist,
+			cache: func() Cache {
+				bm := NewMemoryCache(1 * time.Second)
+				return bm
+			}(),
+		},
+		{
+			name: "key expire",
+			key:  "key1",
+			cache: func() Cache {
+				bm := NewMemoryCache(20 * time.Second)
+				err := bm.Set(context.Background(), "key1", "value1", 1*time.Second)
+				time.Sleep(2 * time.Second)
+				assert.Nil(t, err)
+				return bm
+			}(),
+			wantErr: ErrKeyExpired,
+		},
+		{
+			name:  "get val",
+			key:   "key2",
+			value: "author",
+			cache: func() Cache {
+				bm := NewMemoryCache(1 * time.Second)
+				err := bm.Set(context.Background(), "key2", "author", 5*time.Second)
+				assert.Nil(t, err)
+				return bm
+			}(),
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			val, err := tc.cache.Get(context.Background(), tc.key)
+			assert.Equal(t, tc.wantErr, err)
+			if err != nil {
+				return
+			}
+			assert.Equal(t, tc.value, val)
+		})
+	}
+}
+
+func TestMemoryCacheConcurrencyIncr(t *testing.T) {
+	bm := NewMemoryCache(20)
+	err := bm.Set(context.Background(), "cacheIncr", 0, time.Second*20)
+	assert.Nil(t, err)
+	wg := sync.WaitGroup{}
+	wg.Add(10)
+	for i := 0; i < 10; i++ {
+		go func() {
+			defer wg.Done()
+			_ = bm.Increment(context.Background(), "cacheIncr", 1)
+		}()
+	}
+	wg.Wait()
+	val, _ := bm.Get(context.Background(), "cacheIncr")
+	if val.(int) != 10 {
+		t.Error("Incr err")
+	}
+}
